9c339e12b37ad67e6e7c14605c3f0598a1aa45c5
Sushant Mohite <69734778+Darkseidsam7@users.noreply.github.com>
2021-04-08 14:31:05 +0530

    Update hms.sql

--- hms.sql
+++ hms.sql
@@ -18,154 +18,229 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `waterbilling`
+-- Database: `hms`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `bill`
+-- Table structure for table `admin`
 --
 
-CREATE TABLE `bill` (
-  `id` int(10) NOT NULL,
-  `owners_id` int(10) NOT NULL,
-  `prev` varchar(20) NOT NULL,
-  `pres` varchar(20) NOT NULL,
-  `price` varchar(20) NOT NULL,
-  `date` varchar(20) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `bill`
---
-
-INSERT INTO `bill` (`id`, `owners_id`, `prev`, `pres`, `price`, `date`) VALUES
-(1, 1, '56', '78', '10', '16/02/04 03:28:20'),
-(2, 1, '78', '67', '10', '16/02/04 03:42:03'),
-(3, 1, '67', '67', '0', '16/02/04 03:52:51'),
-(4, 1, '67', '80', '130', '16/02/04 03:53:44'),
-(5, 1, '80', '100', '200', '16/02/04 03:54:19'),
-(6, 1, '100', '500', '4000', '16/02/04 08:15:33'),
-(7, 2, '122', '500', '3780', '18/09/11 14:23:49'),
-(8, 3, '100', '01', '-990', '18/09/17 16:18:21');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `owners`
---
-
-CREATE TABLE `owners` (
-  `id` int(10) NOT NULL,
-  `lname` varchar(60) NOT NULL,
-  `fname` varchar(60) NOT NULL,
-  `mi` varchar(2) NOT NULL,
-  `address` varchar(60) NOT NULL,
-  `contact` varchar(15) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `owners`
---
-
-INSERT INTO `owners` (`id`, `lname`, `fname`, `mi`, `address`, `contact`) VALUES
-(1, 'Eda', 'kimei', '90', 'Mbinga', '0654100235');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `tempo_bill`
---
-
-CREATE TABLE `tempo_bill` (
+CREATE TABLE `admin` (
   `id` int(11) NOT NULL,
-  `Prev` varchar(40) NOT NULL,
-  `Client` varchar(30) NOT NULL
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `updationDate` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `tempo_bill`
+-- Dumping data for table `admin`
 --
 
-INSERT INTO `tempo_bill` (`id`, `Prev`, `Client`) VALUES
-(1, '500', 'kimei');
+INSERT INTO `admin` (`id`, `username`, `password`, `updationDate`) VALUES
+(1, 'admin', 'Test@12345', '28-12-2019 11:42:05 AM');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `user`
+-- Table structure for table `appointment`
 --
 
-CREATE TABLE `user` (
-  `id` int(10) NOT NULL,
-  `username` varchar(50) NOT NULL,
-  `password` varchar(60) NOT NULL,
-  `name` varchar(60) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `user`
---
-
-INSERT INTO `user` (`id`, `username`, `password`, `name`) VALUES
-(4, 'admin', 'admin', 'admin');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `user_levels`
---
-
-CREATE TABLE `user_levels` (
+CREATE TABLE `appointment` (
   `id` int(11) NOT NULL,
-  `username` varchar(20) NOT NULL,
-  `password` varchar(20) NOT NULL,
-  `userlevel` varchar(20) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `doctorSpecialization` varchar(255) NOT NULL,
+  `doctorId` int(11) NOT NULL,
+  `userId` int(11) NOT NULL,
+  `consultancyFees` int(11) NOT NULL,
+  `appointmentDate` varchar(255) NOT NULL,
+  `appointmentTime` varchar(255) NOT NULL,
+  `postingDate` timestamp NOT NULL DEFAULT current_timestamp(),
+  `userStatus` int(11) NOT NULL,
+  `doctorStatus` int(11) NOT NULL,
+  `updationDate` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `user_levels`
+-- Dumping data for table `appointment`
 --
 
-INSERT INTO `user_levels` (`id`, `username`, `password`, `userlevel`) VALUES
-(1, 'user1', 'user1', '1'),
-(2, 'user2', 'user2', '2'),
-(3, 'user3', 'user3', '3'),
-(4, 'user4', 'user4', '4');
+INSERT INTO `appointment` (`id`, `doctorSpecialization`, `doctorId`, `userId`, `consultancyFees`, `appointmentDate`, `appointmentTime`, `postingDate`, `userStatus`, `doctorStatus`, `updationDate`) VALUES
+(1, 'Dentist', 1, 1, 500, '2016-12-31', '09:25', '2017-01-01 00:29:02', 1, 0, ''),
+(2, 'Homeopath', 4, 5, 700, '2017-01-11', '14:10', '2017-01-07 08:02:58', 0, 1, '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `doctors`
+--
+
+CREATE TABLE `doctors` (
+  `id` int(11) NOT NULL,
+  `specilization` varchar(255) NOT NULL,
+  `doctorName` varchar(255) NOT NULL,
+  `address` longtext NOT NULL,
+  `docFees` varchar(255) NOT NULL,
+  `contactno` bigint(11) NOT NULL,
+  `docEmail` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `creationDate` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updationDate` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `doctors`
+--
+
+INSERT INTO `doctors` (`id`, `specilization`, `doctorName`, `address`, `docFees`, `contactno`, `docEmail`, `password`, `creationDate`, `updationDate`) VALUES
+(1, 'Dentist', 'Sohail', 'Pune', '500', 987654321, 'sohail@m.com', '5c428d8875d2948607f3e3fe134d71b4', '2016-12-29 06:25:37', '04-01-2017 01:27:51 PM'),
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `doctorslog`
+--
+
+CREATE TABLE `doctorslog` (
+  `id` int(11) NOT NULL,
+  `uid` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `userip` binary(16) NOT NULL,
+  `loginTime` timestamp NOT NULL DEFAULT current_timestamp(),
+  `logout` varchar(255) NOT NULL,
+  `status` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `doctorslog`
+--
+
+INSERT INTO `doctorslog` (`id`, `uid`, `username`, `userip`, `loginTime`, `logout`, `status`) VALUES
+(1, 2, 'admin', 0x30000000000000000000000000000000, '2017-01-06 05:53:31', '', 1),
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `doctorspecilization`
+--
+
+CREATE TABLE `doctorspecilization` (
+  `id` int(11) NOT NULL,
+  `specilization` varchar(255) NOT NULL,
+  `creationDate` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updationDate` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `doctorspecilization`
+--
+
+INSERT INTO `doctorspecilization` (`id`, `specilization`, `creationDate`, `updationDate`) VALUES
+(1, 'Gynecologist/Obstetrician', '2016-12-28 06:37:25', ''),
+(2, 'General Physician', '2016-12-28 06:38:12', ''),
+(3, 'Dermatologist', '2016-12-28 06:38:48', ''),
+(4, 'Homeopath', '2016-12-28 06:39:26', ''),
+(5, 'Ayurveda', '2016-12-28 06:39:51', ''),
+(6, 'Dentist', '2016-12-28 06:40:08', ''),
+(7, 'Ear-Nose-Throat (Ent) Specialist', '2016-12-28 06:41:18', ''),
+(9, 'Demo test', '2016-12-28 07:37:39', '28-12-2016 01:28:42 PM'),
+(10, 'Bones Specialist demo', '2017-01-07 08:07:53', '07-01-2017 01:38:04 PM');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `userlog`
+--
+
+CREATE TABLE `userlog` (
+  `id` int(11) NOT NULL,
+  `uid` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `userip` binary(16) NOT NULL,
+  `loginTime` timestamp NOT NULL DEFAULT current_timestamp(),
+  `logout` varchar(255) NOT NULL,
+  `status` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `userlog`
+--
+
+INSERT INTO `userlog` (`id`, `uid`, `username`, `userip`, `loginTime`, `logout`, `status`) VALUES
+(6, 1, '', 0x3a3a3100000000000000000000000000, '2017-01-06 07:02:28', '', 1),
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `fullName` varchar(255) NOT NULL,
+  `address` longtext NOT NULL,
+  `city` varchar(255) NOT NULL,
+  `gender` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `regDate` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updationDate` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `fullName`, `address`, `city`, `gender`, `email`, `password`, `regDate`, `updationDate`) VALUES
+(1, 'Sohail M', 'Test address', 'Karve Nagar', 'Pune', 'info@wfreelance.com', 'f925916e2754e5e03f75dd58a5733251', '2016-12-26 07:03:09', '30-12-2016 11:27:47 AM'),
+
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `bill`
+-- Indexes for table `admin`
 --
-ALTER TABLE `bill`
+ALTER TABLE `admin`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `owners`
+-- Indexes for table `appointment`
 --
-ALTER TABLE `owners`
+ALTER TABLE `appointment`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `tempo_bill`
+-- Indexes for table `doctors`
 --
-ALTER TABLE `tempo_bill`
+ALTER TABLE `doctors`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `user`
+-- Indexes for table `doctorslog`
 --
-ALTER TABLE `user`
+ALTER TABLE `doctorslog`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `user_levels`
+-- Indexes for table `doctorspecilization`
 --
-ALTER TABLE `user_levels`
+ALTER TABLE `doctorspecilization`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `userlog`
+--
+ALTER TABLE `userlog`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -173,34 +248,46 @@ ALTER TABLE `user_levels`
 --
 
 --
--- AUTO_INCREMENT for table `bill`
+-- AUTO_INCREMENT for table `admin`
 --
-ALTER TABLE `bill`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+ALTER TABLE `admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT for table `owners`
+-- AUTO_INCREMENT for table `appointment`
 --
-ALTER TABLE `owners`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+ALTER TABLE `appointment`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT for table `tempo_bill`
+-- AUTO_INCREMENT for table `doctors`
 --
-ALTER TABLE `tempo_bill`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+ALTER TABLE `doctors`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
--- AUTO_INCREMENT for table `user`
+-- AUTO_INCREMENT for table `doctorslog`
 --
-ALTER TABLE `user`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+ALTER TABLE `doctorslog`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
--- AUTO_INCREMENT for table `user_levels`
+-- AUTO_INCREMENT for table `doctorspecilization`
 --
-ALTER TABLE `user_levels`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+ALTER TABLE `doctorspecilization`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `userlog`
+--
+ALTER TABLE `userlog`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
